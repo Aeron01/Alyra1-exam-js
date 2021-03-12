c3fcc74b0eef7a420ff383a3e0746dfde5911cca
Aeron01 <one.soulreaper@gmail.com>
2021-03-12 15:16:25 +0100

    done

---
+++ guess.js
@@ -0,0 +1,14 @@
+const guess = (userGuess, secret) => {
+  if (userGuess < secret) {
+    return 'Too small!'
+  } else if (userGuess > secret) {
+    return 'Too big!'
+  } else if (userGuess = secret) {
+    return 'You win'
+  }
+}
+
+
+
+
+console.log(guess(11, 10))
